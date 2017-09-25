34384009958dc6bc5213b9b34436596c94b23e3b
supermay <wu_jiamei@outlook.com>
2017-09-25 16:34:58 +0200

    Background Images rendered

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,9 +1,16 @@
 import React, { PureComponent } from 'react';
 
 class Background extends PureComponent {
+  renderBackground = (background,index) => {
+    return (
+      <img key={index} src={background.image} alt={background.id}/>
+    )
+  }
   render(){
     return (
-      <p>Background</p>
+      <div className="background">
+        {this.props.backgrounds.map(this.renderBackground)}
+      </div>
     )
   }
 }
