f5e49c90eb6e466c3c7270cedaa0344f7a80ff5d
supermay <wu_jiamei@outlook.com>
2017-09-25 16:12:48 +0200

    Setup Frontend

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Background from './components/Background'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>QueRico Woerden</h1>
+        <Background />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Background.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+
+class Background extends PureComponent {
+  render(){
+    return (
+      <p>Background</p>
+    )
+  }
+}
+
+export default Background;
